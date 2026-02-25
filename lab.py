f35e24a439e8c58237394e4385d9cbf1339df0d1
bluundyk <fromenlesan2@gmail.com>
2026-02-25 20:06:34 +0400

    task 3 has been completed, and the original expression has been written down. Since expanding the parentheses only complicates the expression, it is already written in a simple form. This can be seen from the repeated output

--- lab.py
+++ lab.py
@@ -1,7 +1,26 @@
 import numpy as np
+import math
+from sympy import *
 
+#1
 numbers = np.random.uniform(-3 + 1e-15, 3, (5, 5))
 numbers.transpose()
 determinant = np.linalg.det(numbers)
 
 print(determinant)
+
+#3
+x, y = symbols('x y')
+expr = (7*x*y)/4 * (x + y) - (x - y)**2
+print("=============Ваше выражение===========")
+pprint(expr)
+print("==========Упрощённое выражение========")
+new_expr = simplify(expr)
+pprint(new_expr)
+print("===============Результат==============")
+
+val_x = -1.23
+val_y = math.sqrt(8)
+res = new_expr.subs({x: val_x, y: val_y})
+print(res)
+
